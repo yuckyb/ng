3840e91f327e4989b96a64d6a9021390c902cc7f
yang_bing <test49@test49.com>
2018-11-20 17:24:37 +0800

    创建服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,28 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+
+  /**
+   * 
+   * @param heroServeice 
+   * 注入 HeroService
+   * 这个参数同时做了两件事：1. 声明了一个私有 heroService 属性，
+   * 2. 把它标记为一个 HeroService 的注入点。
+   */
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
+
